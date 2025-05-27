aa378d6f6a1ee838ce25c9e32b27e64bf5972796
Shivam Shukla <116748063+Shivam-Shukl@users.noreply.github.com>
2025-05-27 07:08:05 +0530

    Time: 0 ms (100%), Space: 17.9 MB (73.58%) - LeetHub

---
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findSubarrays(self, nums: List[int]) -> bool:
+        n = len(nums)
+        seen_sum = set()
+
+        for i in range(n-1):
+            pair_sum = nums[i] + nums[i+1]
+
+            if pair_sum  in seen_sum:
+                return True
+            
+            seen_sum.add(pair_sum)
+        return False;
+        
