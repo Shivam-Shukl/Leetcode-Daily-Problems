94862379611c42dcb29405323710b8ba2f7d4736
Shivam Shukla <116748063+Shivam-Shukl@users.noreply.github.com>
2025-06-03 07:56:23 +0530

    Time: 12 ms (75.21%), Space: 94.2 MB (83.75%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -26,6 +26,38 @@ public:
 
         return dp[index][swapped] = ans;
     }
+    int solveSpace(vector<int>& nums1, vector<int>& nums2) {
+        
+        int n = nums1.size();
+     
+        vector<int> next(2, 0);
+        vector<int> curr(2,0);
+
+        for(int index = n-1;index>=1;index--)
+        {
+            for(int swapped = 0 ;swapped<=1 ;swapped++)
+            {
+                int ans = INT_MAX;
+                int prev1 = nums1[index - 1];
+                int prev2 = nums2[index - 1];
+
+                // catch
+                if (swapped)
+                    swap(prev1, prev2);
+
+                if (nums1[index] > prev1 && nums2[index] > prev2)
+                    ans = next[0];
+
+                // swap
+                if (nums1[index] > prev2 && nums2[index] > prev1)
+                    ans = min(ans, 1 + next[1]);
+
+                curr[swapped] = ans;
+            }
+            next = curr;
+        }
+        return curr[0];
+    }
     int solveTab(vector<int>& nums1, vector<int>& nums2) {
         
         int n = nums1.size();
@@ -66,6 +98,8 @@ public:
         // bool swapped = 0;
         // return solve(nums1, nums2, 1, swapped, dp);
 
-        return solveTab(nums1,nums2);
+        // return solveTab(nums1,nums2);
+
+        return solveSpace(nums1,nums2);
     }
 };
