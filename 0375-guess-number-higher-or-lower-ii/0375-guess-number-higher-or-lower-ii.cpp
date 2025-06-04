af88048d5baa41919482a5e627a07d0b5c454276
Shivam Shukla <116748063+Shivam-Shukl@users.noreply.github.com>
2025-06-04 20:21:26 +0530

    Time: 81 ms (39.47%), Space: 11.2 MB (23.34%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int start ,int end)
+    int solve(int start ,int end,vector<vector<int>> &dp)
     {
         // base case
         if(start >= end)
@@ -8,17 +8,55 @@ public:
             return 0;
         }
 
+        if(dp[start][end]!= -1)
+        {
+            return dp[start][end];
+        }
+
         int ans = INT_MAX;
         for(int i=start ;i<= end;i++)
         {
-            int left = solve(start,i-1);
-            int right = solve(i+1,end);
+            int left = solve(start,i-1,dp);
+            int right = solve(i+1,end,dp);
             int val = i + max(left,right);
             ans = min(ans,val);
         }
-        return ans;
+        return dp[start][end] = ans;
     }
+
+    int solveTab(int n)
+    {
+
+        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
+        // base case
+
+        for(int start = n ;start>=1;start--)
+        {
+            for(int end=0 ;end <=n;end++)
+            {
+                if(start == end)
+                {
+                    continue;
+                }else{
+                    int ans = INT_MAX;
+                    for(int i=start ;i<= end;i++)
+                    {
+                        int left = solve(start,i-1,dp);
+                        int right = solve(i+1,end,dp);
+                        int val = i + max(left,right);
+                        ans = min(ans,val);
+                    }
+                    dp[start][end] = ans;
+                }
+            }
+        }
+        
+        return dp[1][n];
+    }
+
     int getMoneyAmount(int n) {
-        return solve(1,n);   
+        // vector<vector<int>> dp(n+1, vector<int>(n+1,-1));
+        // return solve(1,n,dp);   
+        return solveTab(n);
     }
 };
