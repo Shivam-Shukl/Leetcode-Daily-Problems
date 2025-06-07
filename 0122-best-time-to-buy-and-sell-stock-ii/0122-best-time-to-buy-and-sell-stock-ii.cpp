7cfb76130ca93eab1fd888325712c4e455b435e1
Shivam Shukla <116748063+Shivam-Shukl@users.noreply.github.com>
2025-06-07 19:20:45 +0530

    Time: 1 ms (37.17%), Space: 17.3 MB (35.33%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -37,8 +37,6 @@ public:
         int n = prices.size();
         vector<vector<int>> dp(n+1,vector<int>(2,0));
 
-        
-        
         for(int index = n-1;index>=0;index--)
         {
             for(int buy = 0 ;buy<2;buy++)
@@ -64,10 +62,46 @@ public:
         return dp[0][1];
     }
 
+    int solveSpace(vector<int>& prices)
+    {
+
+        int n = prices.size();
+    
+
+        vector<int> next(2,0);
+        vector<int> curr(2,0);
+
+        for(int index = n-1;index>=0;index--)
+        {
+            for(int buy = 0 ;buy<2;buy++)
+            {
+                int profit = 0;
+                if(buy==1)
+                {
+                    int buykaro =  -prices[index] + next[0];
+                    int skipkaro = next[1];
+                    profit = max(buykaro,skipkaro);
+                    
+                }
+                else{
+
+                    int sellkaro = prices[index] + next[1];
+                    int skipkaro = next[0];
+                    profit = max(sellkaro,skipkaro);
+
+                }
+                curr[buy] = profit;
+            }
+            next = curr;
+        }
+        return curr[1];
+    }
+
     int maxProfit(vector<int>& prices) {
         // int n = prices.size();
         // vector<vector<int>> dp(n+1,vector<int>(2,INT_MIN));
         // return solve(prices,1,0,dp);
-        return solveTab(prices);
+        // return solveTab(prices);
+        return solveSpace(prices);
     }
 };
