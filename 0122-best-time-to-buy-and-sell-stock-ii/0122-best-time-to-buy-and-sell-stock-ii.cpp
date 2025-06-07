583b96ca473e25cb8ef4e8e445f1053f3644e136
Shivam Shukla <116748063+Shivam-Shukl@users.noreply.github.com>
2025-06-07 19:07:38 +0530

    Time: 7 ms (18.36%), Space: 19.7 MB (28.27%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -18,18 +18,56 @@ public:
             int buykaro =  -prices[index] + solve(prices,0,index+1,dp);
             int skipkaro = solve(prices ,1, index+1,dp);
             profit = max(buykaro,skipkaro);
+            
         }
         else{
+
             int sellkaro = prices[index] + solve(prices,1,index+1,dp);
             int skipkaro = solve(prices,0,index+1,dp);
             profit = max(sellkaro,skipkaro);
+
         }
         return dp[index][buy] = profit;
 
     }
-    int maxProfit(vector<int>& prices) {
+
+    int solveTab(vector<int>& prices)
+    {
+
         int n = prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,INT_MIN));
-        return solve(prices,1,0,dp);
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+
+        
+        
+        for(int index = n-1;index>=0;index--)
+        {
+            for(int buy = 0 ;buy<2;buy++)
+            {
+                int profit = 0;
+                if(buy==1)
+                {
+                    int buykaro =  -prices[index] + dp[index+1][0];
+                    int skipkaro = dp[index+1][1];
+                    profit = max(buykaro,skipkaro);
+                    
+                }
+                else{
+
+                    int sellkaro = prices[index] +dp[index+1][1]; 
+                    int skipkaro = dp[index+1][0];
+                    profit = max(sellkaro,skipkaro);
+
+                }
+                dp[index][buy] = profit;
+            }
+        }
+        return dp[0][1];
+    }
+
+    int maxProfit(vector<int>& prices) {
+        // int n = prices.size();
+        // vector<vector<int>> dp(n+1,vector<int>(2,INT_MIN));
+        // return solve(prices,1,0,dp);
+        return solveTab(prices);
     }
 };
