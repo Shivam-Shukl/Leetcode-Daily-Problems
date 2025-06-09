915bd891ef7c99d12f5eae02b21f968589170c19
Shivam Shukla <116748063+Shivam-Shukl@users.noreply.github.com>
2025-06-09 08:09:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,102 +1,28 @@
-
-#include <vector>
-#include <climits>
-using namespace std;
-
 class Solution {
 public:
-    int solve(vector<int>& prices, int buy, int index, int val,int k, vector<vector<vector<int>>> &dp) {
-        
-        if (index == prices.size() || val == k)
+    int solve(int index , int operationNo, int k, vector<int>& prices)
+    {
+        if(index == prices.size())
             return 0;
 
-        if (dp[index][buy][val] != INT_MIN)
-            return dp[index][buy][val];
-
+        if(operationNo == 2*k)
+            return 0;
+        
         int profit = 0;
-        if (buy == 1) {
-            int buykaro = -prices[index] + solve(prices, 0, index + 1, val,k, dp);
-            int skipkaro = solve(prices, 1, index + 1, val,k, dp);
+        if(operationNo %2 == 0)
+        {
+            int buykaro = -prices[index] + solve(index+1,operationNo+1,k,prices);
+            int skipkaro = solve(index+1,operationNo , k ,prices);
             profit = max(buykaro, skipkaro);
-        } else {
-            int sellkaro = prices[index] + solve(prices, 1, index + 1, val + 1,k, dp);
-            int skipkaro = solve(prices, 0, index + 1, val, k,dp);
+        }
+        else{
+            int sellkaro = prices[index] + solve(index+1,operationNo+1,k,prices);
+            int skipkaro = solve(index+1,operationNo,k,prices);
             profit = max(sellkaro, skipkaro);
         }
-
-        return dp[index][buy][val] = profit;
+        return profit;
     }
-
-    int solveTab(vector<int>& prices,int k) {
-        int n = prices.size();
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(k+1, 0)));
-        
-        for (int index = n - 1; index >= 0; index--) {
-            for (int buy = 0; buy <= 1; buy++) {
-                for (int val = 0; val <= k; val++) {  // We can do at most 2 transactions (0 and 1 done)
-                    int profit = 0;
-
-                    if (buy == 1) {
-                        int buykaro = -prices[index] + dp[index + 1][0][val];
-                        int skipkaro = dp[index + 1][1][val];
-                        profit = max(buykaro, skipkaro);
-                    } else {
-                        int sellkaro = 0;
-                        if (val + 1 <= k) {  // Only sell if we have transactions left
-                            sellkaro = prices[index] + dp[index + 1][1][val + 1];
-                        }
-                        int skipkaro = dp[index + 1][0][val];
-                        profit = max(sellkaro, skipkaro);
-                    }
-
-                    dp[index][buy][val] = profit;
-                }
-            }
-        }
-
-        return dp[0][1][0];
-    }
-
-    int solveSpace(vector<int>& prices , int k) {
-        int n = prices.size();
-      
-        vector<vector<int>> curr(2, vector<int>(k+1, 0));
-        vector<vector<int>> next(2, vector<int>(k+1, 0));
-
-        for (int index = n - 1; index >= 0; index--) {
-            for (int buy = 0; buy <= 1; buy++) {
-                for (int val = 0; val <= k; val++) {  
-                    int profit = 0;
-
-                    if (buy == 1) {
-                        int buykaro = -prices[index] + next[0][val];
-                        int skipkaro = next[1][val];
-                        profit = max(buykaro, skipkaro);
-                    } else {
-                        int sellkaro = 0;
-                        if (val + 1 <= k) {  // Only sell if we have transactions left
-                            sellkaro = prices[index] + next[1][val + 1];
-                        }
-                        int skipkaro = next[0][val];
-                        profit = max(sellkaro, skipkaro);
-                    }
-
-                    curr[buy][val] = profit;
-                }
-                next = curr;
-            }
-        }
-
-        return curr[1][0];
-    }
-
     int maxProfit(int k, vector<int>& prices) {
-        // int n = prices.size();
-        // vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(k+1, INT_MIN)));
-        // return solve(prices, 1, 0, 0,k, dp);
-
-        // return solveTab(prices,k);
-
-        return solveSpace(prices,k);
+        return solve(0,0,k,prices);
     }
 };
