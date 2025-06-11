dea1d0c9524b0db58674c776826d8c449c181026
Shivam Shukla <116748063+Shivam-Shukl@users.noreply.github.com>
2025-06-11 08:00:10 +0530

    Time: 7 ms (60.69%), Space: 15.8 MB (13.7%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
-    long long solve(string& word1, string& word2, int ind1, int ind2, vector<vector<long long>> &dp) {
-        if (ind1 == word1.size()) return word2.size() - ind2;
-        if (ind2 == word2.size()) return word1.size() - ind1;
+    long long solve(string& word1, string& word2, int ind1, int ind2, vector<vector<long long>> &dp) 
+    {
+        if (ind1 == word1.size())
+             return word2.size() - ind2;
+        if (ind2 == word2.size()) 
+            return word1.size() - ind1;
 
-        if (dp[ind1][ind2] != -1) return dp[ind1][ind2];
+        if (dp[ind1][ind2] != -1) 
+            return dp[ind1][ind2];
 
         if (word1[ind1] == word2[ind2]) {
             return dp[ind1][ind2] = solve(word1, word2, ind1 + 1, ind2 + 1, dp);
