47e0b340aa8ff1f49ae83aef330ae1105a4a5faf
Shivam Shukla <116748063+Shivam-Shukl@users.noreply.github.com>
2025-09-12 19:47:01 +0530

    Time: 7 ms (20.48%), Space: 9.7 MB (48.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size())
-            return false;
-        vector<int> fir(26,0);
-        vector<int> sec(26,0);
-        for(int i=0;i<s.size();i++)
-        {
-            char ch1 = s[i],ch2 = t[i];
-            fir[ch1 -'a']+= 1;
-            sec[ch2 -'a'] += 1; 
-        }
-        for(int i=0;i<26;i++)
-        {
-            if(fir[i] != sec[i])
-                return false;
-        }
-        return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s == t;
     }
 };
