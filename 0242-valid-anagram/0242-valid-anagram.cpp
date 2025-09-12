6f0b905907cc9c48f91f4ea9377b6db7d56557b6
Shivam Shukla <116748063+Shivam-Shukl@users.noreply.github.com>
2025-09-12 19:50:47 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s == t;
+        vector<int> ans1(26,0);
+        vector<int> ans2(26,0);
+        for(char ch : s){
+            ans1[ch-'a']++;
+        }
+        for(char ch : t){
+            ans2[ch - 'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(ans1[i] != ans2[i]){
+                return false;
+            }
+        }
+        return true;
     }
 };
