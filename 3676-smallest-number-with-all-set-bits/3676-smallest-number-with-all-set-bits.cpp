9f1272f6aadc344103bbbf2471871fc6b6c55bd4
Shivam Shukla <shivamshuklass661@gmail.com>
2025-10-31 15:28:46 +0530

    Time: 0 ms (100%), Space: 8.3 MB (27.36%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+    int pow(int base , int exp){
+        int ans = 1;
+        for(int i=0;i<exp;i++){
+            ans = ans*base;
+        }
+        return ans;
+    }
     int smallestNumber(int n) {
         // 1 , 3 , 7, 15, 2^n -1  n is from 1 
         int i = 1;
