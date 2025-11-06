1326686c0515068ce851f1e8b9e22eccfceeb496
Shivam Shukla <shivamshuklass661@gmail.com>
2025-11-06 20:39:28 +0530

    Time: 0 ms (100%), Space: 10.9 MB (6.08%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,31 +1,40 @@
 class Solution {
 public:
-    bool palindrome(string s){
+    bool palindrome(string s) {
         int n = s.size();
-        int i = 0,j= n-1;
-        while(j>= i){
-            if(s[i] != s[j]){
+        int i = 0, j = n - 1;
+        while (j >= i) {
+            if (s[i] != s[j]) {
                 return false;
             }
-            i++,j--;
+            i++, j--;
         }
         return true;
     }
+    bool isalnum(char ch) {
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') ||
+            (ch >= '0' && ch <= '9')) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     bool isPalindrome(string s) {
         int n = s.size();
         string str = "";
-        for(int i=0;i<n;i++){
-            if(isalnum(s[i])){
+        for (int i = 0; i < n; i++) {
+            if (isalnum(s[i])) {
                 char ch = s[i];
-                if( s[i] >= 'A' && s[i] <= 'Z' ){
+                if (s[i] >= 'A' && s[i] <= 'Z') {
                     ch = ch - 'A' + 'a';
                 }
                 str += ch;
-            }else{
+            } else {
                 continue;
             }
         }
-        
+
         return palindrome(str);
     }
 };
