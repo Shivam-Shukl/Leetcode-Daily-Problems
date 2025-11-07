84388e366de192dbaf6347d6f290c3c51cccd20e
Shivam Shukla <shivamshuklass661@gmail.com>
2025-11-07 11:35:11 +0530

    Time: 8 ms (80.23%), Space: 11.7 MB (96.28%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        // warshall algorithm 
+        // slow , fast node
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(slow && slow -> next && fast->next && fast->next ->next){
+            slow = slow -> next;
+            fast = fast -> next ->next;
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+};
