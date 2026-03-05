350ced61af046dd74213a5d01f9812874f6e71ed
Shivam Shukla <shivamshuklass661@gmail.com>
2026-03-05 10:12:46 +0530

    Time: 3 ms (17.69%), Space: 9.6 MB (14.85%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
-    int minOperations(string s) {
-        int n = s.size();
-
-        string temp = s;
-        // first method s[i] = '1'
+    int solve(string temp, int bit) {
         int cnt = 0;
-        if (temp[0] == '0') {
-            cnt++;
-        }
-        int bit = 0;
+        int n = temp.size();
         for (int i = 1; i < n; i++) {
             if ((temp[i] == '0' && bit == 1) || (temp[i] == '1' && bit == 0)) {
                 cnt++;
@@ -19,21 +12,24 @@ public:
             else
                 bit = 0;
         }
+        return cnt;
+    }
+    int minOperations(string s) {
+        int n = s.size();
+
+        // first method s[i] = '1'
+        int cnt = 0;
+        if (s[0] == '0') {
+            cnt++;
+        }
+        cnt = cnt + solve(s, 0);
 
         int val = 0;
-        if (temp[0] == '1') {
+        if (s[0] == '1') {
             val++;
         }
-        bit = 1;
-        for (int i = 1; i < n; i++) {
-            if ((temp[i] == '0' && bit == 1) || (temp[i] == '1' && bit == 0)) {
-                val++;
-            }
-            if (bit == 0)
-                bit = 1;
-            else
-                bit = 0;
-        }
+        val = val + solve(s, 1);
+
         return min(val, cnt);
     }
 };
