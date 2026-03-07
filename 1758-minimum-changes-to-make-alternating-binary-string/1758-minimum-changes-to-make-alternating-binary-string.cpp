39a725756cbf66e4b43d3fae33c44d8b69823c33
Shivam Shukla <shivamshuklass661@gmail.com>
2026-03-07 16:36:12 +0530

    Time: 0 ms (100%), Space: 9.5 MB (19.64%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    int solve(string temp, int bit) {
-        int cnt = 0;
-        int n = temp.size();
-        for (int i = 1; i < n; i++) {
-            if ((temp[i] == '0' && bit == 1) || (temp[i] == '1' && bit == 0)) {
-                cnt++;
-            }
-            if (bit == 0)
-                bit = 1;
-            else
-                bit = 0;
-        }
-        return cnt;
-    }
     int minOperations(string s) {
         int n = s.size();
 
-        // first method s[i] = '1'
-        int cnt = 0;
-        if (s[0] == '0') {
-            cnt++;
-        }
-        cnt = cnt + solve(s, 0);
+        int zero_at_even = 0;
+        int zero_at_odd = 0;
+        int one_at_even = 0;
+        int one_at_odd = 0;
 
-        int val = 0;
-        if (s[0] == '1') {
-            val++;
-        }
-        val = val + solve(s, 1);
+        for (int i = 0; i < n; i++) {
 
-        return min(val, cnt);
+            if (i % 2 == 0) {
+                if (s[i] == '0')
+                    zero_at_even++;
+                else
+                    one_at_even++;
+            } else {
+                if (s[i] == '1')
+                    one_at_odd++;
+                else
+                    zero_at_odd++;
+            }
+        }
+
+        // configuration 1: 1 at even, 0 at odd
+        // configuration 2: 0 at even, 1 at odd
+        return min(zero_at_even + one_at_odd, zero_at_odd + one_at_even);
     }
 };
