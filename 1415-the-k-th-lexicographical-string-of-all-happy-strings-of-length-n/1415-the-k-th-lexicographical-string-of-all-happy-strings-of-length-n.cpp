60a3af56c40561a3ada5370d67f28c54b81cd343
Shivam Shukla <shivamshuklass661@gmail.com>
2026-03-14 19:44:32 +0530

    Time: 43 ms (16.4%), Space: 26.8 MB (10.34%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
-    void solve(int n,vector<string>& ans,string &s){
-        if(n== 0){
+    void solve(int n, char last, vector<string>& ans, string &s){
+        if(n == 0){
             ans.push_back(s);
             return;
         }
 
-        if(s.size() == 0 ||(s.size() >= 0 && s.back()  != 'a')){
+        if(last == '#' || last != 'a'){
             s.push_back('a');
-            solve(n-1,ans,s);
+            solve(n-1, 'a', ans, s);
             s.pop_back();
         }
 
-        if(s.size() == 0 ||(s.size() >= 0 && s.back()  != 'b')){
+        if(last == '#' || last != 'b'){
             s.push_back('b');
-            solve(n-1,ans,s);
+            solve(n-1, 'b', ans, s);
             s.pop_back();
         }
 
-        if(s.size() == 0 ||(s.size() >= 0 && s.back()  != 'c')){
+        if(last == '#' || last != 'c'){
             s.push_back('c');
-            solve(n-1,ans,s);
+            solve(n-1, 'c', ans, s);
             s.pop_back();
         }
-
     }
+
     string getHappyString(int n, int k) {
         vector<string> ans;
         string s = "";
-        solve(n,ans,s);
-        sort(ans.begin(),ans.end());
-        // for(int i =0;i<ans.size();i++) cout<<ans[i]<<" ";
-        // cout<<endl;
-        if(k <= ans.size() )
+        
+        solve(n, '#', ans, s);
+
+        sort(ans.begin(), ans.end());
+
+        if(k <= ans.size()){
             return ans[k-1];
-        else 
-            return "";
+        }
+        return "";
     }
 };
