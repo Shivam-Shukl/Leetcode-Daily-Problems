cc06b6265ca030760fffb9893e0c152681dd9bf1
Shivam Shukla <shivamshuklass661@gmail.com>
2026-04-06 09:04:01 +0530

    Time: 7 ms (46.78%), Space: 19.3 MB (67.45%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool canFinish(int V , vector<vector<int>>& pre) {
+        // 
+        vector<vector<int>> graph(V);
+        vector<int> ind(V,0);
+        for(int i =0;i<pre.size();i++){
+            int u = pre[i][0];
+            int v = pre[i][1];
+            graph[v].push_back(u);
+            ind[u]++;
+        }
+        
+        queue<int> q;
+        for(int i = 0;i<V;i++){
+            if(ind[i] ==0){
+                q.push(i);
+            }
+        }
+
+        vector<int> val;
+        while(!q.empty()){
+            int frontNode = q.front();
+            q.pop();
+            val.push_back(frontNode);
+            for(auto u : graph[frontNode]){
+                ind[u]--;
+                if(ind[u] == 0){
+                    q.push(u);
+                }
+            }
+        }
+
+        return val.size() == V;
+
+    }
+};
